7064d32e40d1a23460a7048dd9953319e972325f
franky <lovelyorg@126.com>
2018-11-20 16:03:56 +0800

    增加修改密码

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-const Koa = require("koa");
-const app = new Koa();
-const path = require("path");
-const serve = require("koa-static");
-const fs = require("fs");
-const route = require("koa-route");
-const bodyparser = require("koa-bodyparser");
+const Koa = require('koa')
+const app = new Koa()
+const path = require('path')
+const serve = require('koa-static')
+const fs = require('fs')
+const route = require('koa-route')
+const bodyparser = require('koa-bodyparser')
 
-Array.prototype.remove = function (val) {
-  var index = this.indexOf(val);
+Array.prototype.remove = function(val) {
+  var index = this.indexOf(val)
   if (index > -1) {
-    this.splice(index, 1);
+    this.splice(index, 1)
   }
-};
+}
 
-app.use(serve(path.join(__dirname)));
+app.use(serve(path.join(__dirname)))
 
 app.use(
   bodyparser({
-    enableTypes: ["json", "form", "text"],
-    formLimit: "10mb"
+    enableTypes: ['json', 'form', 'text'],
+    formLimit: '10mb'
   })
-);
+)
 
 app.use(
-  route.get("/", ctx => {
-    ctx.response.redirect("index.html");
+  route.get('/', ctx => {
+    ctx.response.redirect('index.html')
   })
-);
+)
 
 // app.use(
 //   route.get("/file/:name", async (ctx, name) => {
@@ -36,65 +36,65 @@ app.use(
 // );
 
 app.use(
-  route.put("/:user", async (ctx, user) => {
+  route.put('/:user', async (ctx, user) => {
     if (!ctx.request.body.data) {
-      ctx.body = "无数据";
-      return;
+      ctx.body = '无数据'
+      return
     }
     try {
-      await writeFile(`static/data/${user}/data`, ctx.request.body.data);
-      uploadData();
+      await writeFile(`static/data/${user}/data`, ctx.request.body.data)
+      //uploadData();
     } catch (error) {
-      msg = "更新失败";
+      msg = '更新失败'
     }
-    ctx.body = "";
+    ctx.body = ''
   })
-);
+)
 
-app.listen(3333);
+app.listen(3333)
 
 function uploadData() {
-  var nodeCmd = require("node-cmd");
+  var nodeCmd = require('node-cmd')
 
-  nodeCmd.run(__dirname + "\\upload.bat");
+  nodeCmd.run(__dirname + '\\upload.bat')
 }
 
 function loopDirGetFilename(theDirPath, getfiles) {
-  theDirPath += "/";
-  getfiles = getfiles || [];
-  var files = fs.readdirSync(theDirPath);
+  theDirPath += '/'
+  getfiles = getfiles || []
+  var files = fs.readdirSync(theDirPath)
   files.forEach(filename => {
-    var stats = fs.statSync(theDirPath + filename);
-    if (stats.isFile()) getfiles.push(theDirPath + filename);
+    var stats = fs.statSync(theDirPath + filename)
+    if (stats.isFile()) getfiles.push(theDirPath + filename)
     if (stats.isDirectory())
-      loopDirGetFilename(theDirPath + filename + "/", getfiles);
-  });
-  return getfiles;
+      loopDirGetFilename(theDirPath + filename + '/', getfiles)
+  })
+  return getfiles
 }
 
 async function readFile(filename) {
   return new Promise((s, j) => {
-    fs.readFile(filename, function (err, data) {
-      if (err) j(err);
-      s(data);
-    });
-  });
+    fs.readFile(filename, function(err, data) {
+      if (err) j(err)
+      s(data)
+    })
+  })
 }
 
 async function writeFile(filename, data) {
   return new Promise((s, j) => {
-    fs.writeFile(filename, data, function (err) {
-      if (err) j(err);
-      s();
-    });
-  });
+    fs.writeFile(filename, data, function(err) {
+      if (err) j(err)
+      s()
+    })
+  })
 }
 
 async function deleteFile(filename) {
   return new Promise((s, j) => {
-    fs.unlink(filename, function (err) {
-      if (err) j(err);
-      s();
-    });
-  });
+    fs.unlink(filename, function(err) {
+      if (err) j(err)
+      s()
+    })
+  })
 }
